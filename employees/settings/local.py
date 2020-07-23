766426abf239767c6d84d1fc22f5dd8272285923
noe vaz <jonovagu@gmail.com>
2020-07-23 07:37:12 -0500

    [ADD] added custom directory with files for local and production environmets, use python3 manage.py runserver --settings=employees.settings.local to run the server. remember that local.py and production.py must import everything from base.py

---
+++ employees/settings/local.py
@@ -0,0 +1,19 @@
+from .base import *
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
